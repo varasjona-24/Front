a772b80eac208527177877edce6dc451d3627dc9
varasjona-24 <varas.jona@gmail.com>
2026-01-15 15:27:40 -0500

    ok

---
+++ lib/Modules/sources/domain/source_origin_detector.dart
@@ -0,0 +1,29 @@
+import '../domain/source_origin.dart';
+
+SourceOrigin detectOriginFromUrl(String url) {
+  final u = url.toLowerCase();
+
+  if (u.contains('youtube.com') || u.contains('youtu.be'))
+    return SourceOrigin.youtube;
+  if (u.contains('instagram.com')) return SourceOrigin.instagram;
+  if (u.contains('vimeo.com')) return SourceOrigin.vimeo;
+  if (u.contains('reddit.com')) return SourceOrigin.reddit;
+  if (u.contains('t.me') || u.contains('telegram'))
+    return SourceOrigin.telegram;
+  if (u.contains('x.com') || u.contains('twitter.com')) return SourceOrigin.x;
+  if (u.contains('facebook.com') || u.contains('fb.watch'))
+    return SourceOrigin.facebook;
+  if (u.contains('pinterest.')) return SourceOrigin.pinterest;
+  if (u.contains('aminoapps.com')) return SourceOrigin.amino;
+  if (u.contains('blogspot.') || u.contains('blogger.com'))
+    return SourceOrigin.blogger;
+  if (u.contains('twitch.tv')) return SourceOrigin.twitch;
+  if (u.contains('kick.com')) return SourceOrigin.kick;
+  if (u.contains('snapchat.com')) return SourceOrigin.snapchat;
+  if (u.contains('qq.com')) return SourceOrigin.qq;
+  if (u.contains('threads.net')) return SourceOrigin.threads;
+  if (u.contains('vk.com')) return SourceOrigin.vk;
+  if (u.contains('4chan.org')) return SourceOrigin.chan4;
+
+  return SourceOrigin.generic;
+}
